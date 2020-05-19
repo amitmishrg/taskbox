0ece182425cb9eb54690b9153d83d08f3cd94bfd
amit.mish <amitm@media.net>
2020-05-19 16:08:18 +0530

    taskbox UI

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app','@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions', 
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    './.storybook/design-addon/register.js'
+  ],
 };
